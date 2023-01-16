9dd8a9466ccd0d85d25d087098c9e247cd3666e6
Diez <62673882+zeidfazazi@users.noreply.github.com>
2023-01-16 23:43:33 +0100

    Initial front commit

---
+++ front/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <header className='container'>
+      <h1>Autobill Interface Web</h1>
+    </header>
+     <main className="grid">
+      <section>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+      </section>
+      <section>
+        2
+      </section>
+    </main>
+    </>
+   
+  )
+}
+
+export default App
